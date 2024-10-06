27aa58d567bf2dab2ac3d7ab145372a4948d5bb9
Gabriel Nield <gabrielnield@proton.me>
2024-10-06 10:40:17 -0700

    Add files via upload

---
+++ suckless/dmenu/config.h
@@ -0,0 +1,29 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrainsMono Nerd Font:size=10:style=Bold",
+	"Noto Color Emoji:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cdd6f4", "#11111b" },
+	[SchemeSel] = { "#cba6f7", "#1e1e2e" },
+	[SchemeSelHighlight] = { "#8839ef", "#1e1e2e" },
+	[SchemeNormHighlight] = { "#8839ef", "#11111b" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
